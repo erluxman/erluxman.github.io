7b581881146a2c9588293e2c9b4dc442d01a6d92
Laxman Bhattarai <erluxman@gmail.com>
2020-04-08 22:53:59 +0545

    changed the theme

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -207,7 +207,7 @@ class BlogPostTemplate extends React.Component {
               style={{
                 boxShadow: 'none',
                 textDecoration: 'none',
-                color: 'var(--pink)',
+                color: 'var(textLink)',
               }}
               to={'/'}
             >
